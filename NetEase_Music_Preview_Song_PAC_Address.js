566fd5693e1100fb55a1887611dab9cf1e04ec22
触手TV紫幕 <45619879+IZIMUI@users.noreply.github.com>
2020-03-31 08:42:02 +0800

    Update NetEase_Music_Preview_Song_PAC_Address.js

--- NetEase_Music_Preview_Song_PAC_Address.js
+++ NetEase_Music_Preview_Song_PAC_Address.js
@@ -1,3 +1,421 @@
-require("Encoding_List.js");
-require("Decode_lZiMUl.js");
+function Main(){
 eval(Decode_lZiMUl("[ENUS05]-[ENUS29]-[ENUS27]-[ENUS37]-[ENUS39]-[SPACES]-[ENUS14]-[ENUS28]-[ENUS36]-[ENUS38]-[SYMB12]-[ENUS22]-[ENUS17]-[ENUS37]-[ENUS39]-[SPACES]-[SYMB08]-[SPACES]-[ENUS27]-[ENUS09]-[ENUS45]-[SPACES]-[ENUS00]-[ENUS35]-[ENUS35]-[ENUS01]-[ENUS49]-[SYMB04]-[SYMB13]-[ENUS25]-[ENUS41]-[ENUS37]-[ENUS17]-[ENUS05]-[SYMB09]-[NBZM01]-[NBZM06]-[NBZM03]-[SYMB09]-[ENUS05]-[ENUS29]-[ENUS25]-[SYMB13]-[SYMB14]-[SYMB13]-[ENUS17]-[ENUS27]-[ENUS39]-[ENUS09]-[ENUS35]-[ENUS11]-[ENUS01]-[ENUS05]-[ENUS09]-[SYMB09]-[ENUS25]-[ENUS41]-[ENUS37]-[ENUS17]-[ENUS05]-[SYMB09]-[NBZM01]-[NBZM06]-[NBZM03]-[SYMB09]-[ENUS05]-[ENUS29]-[ENUS25]-[SYMB13]-[SYMB14]-[SYMB13]-[ENUS17]-[ENUS27]-[ENUS39]-[ENUS09]-[ENUS35]-[ENUS11]-[ENUS01]-[ENUS05]-[ENUS09]-[NBZM03]-[SYMB09]-[ENUS25]-[ENUS41]-[ENUS37]-[ENUS17]-[ENUS05]-[SYMB09]-[NBZM01]-[NBZM06]-[NBZM03]-[SYMB09]-[ENUS05]-[ENUS29]-[ENUS25]-[SYMB13]-[SYMB14]-[SYMB13]-[ENUS01]-[ENUS31]-[ENUS25]-[SYMB09]-[ENUS25]-[ENUS41]-[ENUS37]-[ENUS17]-[ENUS05]-[SYMB09]-[NBZM01]-[NBZM06]-[NBZM03]-[SYMB09]-[ENUS05]-[ENUS29]-[ENUS25]-[SYMB13]-[SYMB14]-[SYMB13]-[ENUS01]-[ENUS31]-[ENUS25]-[NBZM03]-[SYMB09]-[ENUS25]-[ENUS41]-[ENUS37]-[ENUS17]-[ENUS05]-[SYMB09]-[NBZM01]-[NBZM06]-[NBZM03]-[SYMB09]-[ENUS05]-[ENUS29]-[ENUS25]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM03]-[NBZM09]-[SYMB09]-[NBZM01]-[NBZM00]-[NBZM05]-[SYMB09]-[NBZM06]-[NBZM03]-[SYMB09]-[NBZM08]-[NBZM00]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM04]-[NBZM07]-[SYMB09]-[NBZM01]-[NBZM00]-[NBZM00]-[SYMB09]-[NBZM01]-[NBZM02]-[NBZM07]-[SYMB09]-[NBZM02]-[NBZM03]-[NBZM09]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM05]-[NBZM09]-[SYMB09]-[NBZM01]-[NBZM01]-[NBZM01]-[SYMB09]-[NBZM02]-[NBZM01]-[SYMB09]-[NBZM01]-[NBZM00]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM05]-[NBZM09]-[SYMB09]-[NBZM01]-[NBZM01]-[NBZM01]-[SYMB09]-[NBZM02]-[NBZM01]-[SYMB09]-[NBZM01]-[NBZM04]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM05]-[NBZM09]-[SYMB09]-[NBZM01]-[NBZM01]-[NBZM01]-[SYMB09]-[NBZM01]-[NBZM06]-[NBZM00]-[SYMB09]-[NBZM01]-[NBZM09]-[NBZM05]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM05]-[NBZM09]-[SYMB09]-[NBZM01]-[NBZM01]-[NBZM01]-[SYMB09]-[NBZM01]-[NBZM06]-[NBZM00]-[SYMB09]-[NBZM01]-[NBZM09]-[NBZM07]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM05]-[NBZM09]-[SYMB09]-[NBZM01]-[NBZM01]-[NBZM01]-[SYMB09]-[NBZM01]-[NBZM08]-[NBZM01]-[SYMB09]-[NBZM03]-[NBZM05]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM05]-[NBZM09]-[SYMB09]-[NBZM01]-[NBZM01]-[NBZM01]-[SYMB09]-[NBZM01]-[NBZM08]-[NBZM01]-[SYMB09]-[NBZM03]-[NBZM08]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM05]-[NBZM09]-[SYMB09]-[NBZM01]-[NBZM01]-[NBZM01]-[SYMB09]-[NBZM01]-[NBZM08]-[NBZM01]-[SYMB09]-[NBZM06]-[NBZM00]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM01]-[NBZM00]-[NBZM03]-[SYMB09]-[NBZM01]-[NBZM02]-[NBZM06]-[SYMB09]-[NBZM09]-[NBZM02]-[SYMB09]-[NBZM01]-[NBZM03]-[NBZM02]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM01]-[NBZM01]-[NBZM02]-[SYMB09]-[NBZM01]-[NBZM03]-[SYMB09]-[NBZM01]-[NBZM02]-[NBZM02]-[SYMB09]-[NBZM01]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM01]-[NBZM01]-[NBZM02]-[SYMB09]-[NBZM01]-[NBZM03]-[SYMB09]-[NBZM01]-[NBZM01]-[NBZM09]-[SYMB09]-[NBZM01]-[NBZM07]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM01]-[NBZM01]-[NBZM05]-[SYMB09]-[NBZM02]-[NBZM03]-[NBZM06]-[SYMB09]-[NBZM01]-[NBZM01]-[NBZM08]-[SYMB09]-[NBZM03]-[NBZM03]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM01]-[NBZM01]-[NBZM05]-[SYMB09]-[NBZM02]-[NBZM03]-[NBZM06]-[SYMB09]-[NBZM01]-[NBZM02]-[NBZM01]-[SYMB09]-[NBZM01]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM01]-[NBZM01]-[NBZM08]-[SYMB09]-[NBZM02]-[NBZM04]-[SYMB09]-[NBZM06]-[NBZM03]-[SYMB09]-[NBZM01]-[NBZM05]-[NBZM06]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM01]-[NBZM09]-[NBZM03]-[SYMB09]-[NBZM01]-[NBZM01]-[NBZM02]-[SYMB09]-[NBZM01]-[NBZM05]-[NBZM09]-[SYMB09]-[NBZM02]-[NBZM02]-[NBZM05]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM02]-[NBZM02]-[NBZM03]-[SYMB09]-[NBZM02]-[NBZM05]-[NBZM02]-[SYMB09]-[NBZM01]-[NBZM09]-[NBZM09]-[SYMB09]-[NBZM06]-[NBZM06]-[SYMB13]-[SYMB14]-[SYMB13]-[NBZM02]-[NBZM02]-[NBZM03]-[SYMB09]-[NBZM02]-[NBZM05]-[NBZM02]-[SYMB09]-[NBZM01]-[NBZM09]-[NBZM09]-[SYMB09]-[NBZM06]-[NBZM07]-[SYMB13]-[SYMB05]-[SYMB11]-[SPACES]-[ENUS11]-[ENUS41]-[ENUS27]-[ENUS05]-[ENUS39]-[ENUS17]-[ENUS29]-[ENUS27]-[SPACES]-[ENUS14]-[ENUS28]-[ENUS36]-[ENUS38]-[SYMB12]-[ENUS18]-[ENUS41]-[ENUS07]-[ENUS13]-[ENUS09]-[ENUS37]-[SYMB04]-[ENUS14]-[ENUS28]-[ENUS36]-[ENUS38]-[SYMB05]-[SPACES]-[SYMB00]-[SPACES]-[ENUS11]-[ENUS29]-[ENUS35]-[SYMB04]-[ENUS23]-[ENUS09]-[ENUS39]-[SPACES]-[ENUS42]-[ENUS01]-[ENUS23]-[ENUS41]-[ENUS09]-[SYMB08]-[NBZM00]-[SYMB11]-[SPACES]-[ENUS42]-[ENUS01]-[ENUS23]-[ENUS41]-[ENUS09]-[SPACES]-[SYMB15]-[SPACES]-[ENUS14]-[ENUS28]-[ENUS36]-[ENUS38]-[SYMB12]-[ENUS22]-[ENUS17]-[ENUS37]-[ENUS39]-[SYMB09]-[ENUS23]-[ENUS09]-[ENUS27]-[ENUS13]-[ENUS39]-[ENUS15]-[SYMB11]-[SPACES]-[ENUS42]-[ENUS01]-[ENUS23]-[ENUS41]-[ENUS09]-[SYMB17]-[SYMB17]-[SYMB05]-[SPACES]-[ENUS17]-[ENUS11]-[SPACES]-[SYMB04]-[ENUS14]-[ENUS28]-[ENUS36]-[ENUS38]-[SPACES]-[SYMB08]-[SYMB08]-[SPACES]-[ENUS14]-[ENUS28]-[ENUS36]-[ENUS38]-[SYMB12]-[ENUS22]-[ENUS17]-[ENUS37]-[ENUS39]-[SYMB02]-[ENUS42]-[ENUS01]-[ENUS23]-[ENUS41]-[ENUS09]-[SYMB03]-[SYMB05]-[SPACES]-[ENUS35]-[ENUS09]-[ENUS39]-[ENUS41]-[ENUS35]-[ENUS27]-[SPACES]-[ENUS39]-[ENUS35]-[ENUS41]-[ENUS09]-[SYMB11]-[SPACES]-[ENUS35]-[ENUS09]-[ENUS39]-[ENUS41]-[ENUS35]-[ENUS27]-[SPACES]-[ENUS11]-[ENUS01]-[ENUS23]-[ENUS37]-[ENUS09]-[SYMB11]-[SPACES]-[SYMB01]-[SYMB11]-[SPACES]-[ENUS11]-[ENUS41]-[ENUS27]-[ENUS05]-[ENUS39]-[ENUS17]-[ENUS29]-[ENUS27]-[SPACES]-[ENUS10]-[ENUS17]-[ENUS27]-[ENUS07]-[ENUS30]-[ENUS35]-[ENUS29]-[ENUS47]-[ENUS49]-[ENUS10]-[ENUS29]-[ENUS35]-[ENUS40]-[ENUS34]-[ENUS22]-[SYMB04]-[ENUS40]-[ENUS34]-[ENUS22]-[SYMB14]-[ENUS14]-[ENUS28]-[ENUS36]-[ENUS38]-[SYMB05]-[SPACES]-[SYMB00]-[SPACES]-[ENUS05]-[ENUS29]-[ENUS27]-[ENUS37]-[ENUS39]-[SPACES]-[ENUS14]-[ENUS28]-[ENUS36]-[ENUS38]-[SYMB12]-[ENUS18]-[ENUS41]-[ENUS07]-[ENUS13]-[ENUS09]-[SPACES]-[SYMB08]-[SPACES]-[ENUS14]-[ENUS28]-[ENUS36]-[ENUS38]-[SYMB12]-[ENUS18]-[ENUS41]-[ENUS07]-[ENUS13]-[ENUS09]-[ENUS37]-[SYMB04]-[ENUS14]-[ENUS28]-[ENUS36]-[ENUS38]-[SYMB05]-[SYMB11]-[SPACES]-[ENUS17]-[ENUS11]-[SPACES]-[SYMB04]-[ENUS14]-[ENUS28]-[ENUS36]-[ENUS38]-[SYMB12]-[ENUS18]-[ENUS41]-[ENUS07]-[ENUS13]-[ENUS09]-[SPACES]-[SYMB08]-[SYMB08]-[SPACES]-[ENUS39]-[ENUS35]-[ENUS41]-[ENUS09]-[SYMB05]-[SPACES]-[ENUS35]-[ENUS09]-[ENUS39]-[ENUS41]-[ENUS35]-[ENUS27]-[SPACES]-[SYMB13]-[ENUS30]-[ENUS34]-[ENUS28]-[ENUS46]-[ENUS48]-[SPACES]-[NBZM01]-[NBZM00]-[NBZM06]-[SYMB09]-[NBZM05]-[NBZM02]-[SYMB09]-[NBZM01]-[NBZM02]-[NBZM07]-[SYMB09]-[NBZM07]-[NBZM02]-[SYMB21]-[NBZM01]-[NBZM09]-[NBZM09]-[NBZM05]-[NBZM01]-[SYMB11]-[SYMB13]-[SYMB11]-[SPACES]-[ENUS09]-[ENUS23]-[ENUS37]-[ENUS09]-[SPACES]-[ENUS17]-[ENUS11]-[SPACES]-[SYMB04]-[ENUS14]-[ENUS28]-[ENUS36]-[ENUS38]-[SYMB12]-[ENUS18]-[ENUS41]-[ENUS07]-[ENUS13]-[ENUS09]-[SPACES]-[SYMB08]-[SYMB08]-[SPACES]-[ENUS11]-[ENUS01]-[ENUS23]-[ENUS37]-[ENUS09]-[SYMB05]-[SPACES]-[ENUS35]-[ENUS09]-[ENUS39]-[ENUS41]-[ENUS35]-[ENUS27]-[SPACES]-[SYMB13]-[ENUS06]-[ENUS16]-[ENUS34]-[ENUS08]-[ENUS04]-[ENUS38]-[SYMB11]-[SYMB13]-[SYMB11]-[SPACES]-[SYMB01]-[SYMB11]"));
+};
+
+//编码内容库
+const Encoding_lZiMUl_List = new Array({
+  "ENUS": [{
+      "Text": "A",
+      "Encoding": "ENUS00"
+    },
+    {
+      "Text": "a",
+      "Encoding": "ENUS01"
+    },
+    {
+      "Text": "B",
+      "Encoding": "ENUS02"
+    },
+    {
+      "Text": "b",
+      "Encoding": "ENUS03"
+    },
+    {
+      "Text": "C",
+      "Encoding": "ENUS04"
+    },
+    {
+      "Text": "c",
+      "Encoding": "ENUS05"
+    },
+    {
+      "Text": "D",
+      "Encoding": "ENUS06"
+    },
+    {
+      "Text": "d",
+      "Encoding": "ENUS07"
+    },
+    {
+      "Text": "E",
+      "Encoding": "ENUS08"
+    },
+    {
+      "Text": "e",
+      "Encoding": "ENUS09"
+    },
+    {
+      "Text": "F",
+      "Encoding": "ENUS10"
+    },
+    {
+      "Text": "f",
+      "Encoding": "ENUS11"
+    },
+    {
+      "Text": "G",
+      "Encoding": "ENUS12"
+    },
+    {
+      "Text": "g",
+      "Encoding": "ENUS13"
+    },
+    {
+      "Text": "H",
+      "Encoding": "ENUS14"
+    },
+    {
+      "Text": "h",
+      "Encoding": "ENUS15"
+    },
+    {
+      "Text": "I",
+      "Encoding": "ENUS16"
+    },
+    {
+      "Text": "i",
+      "Encoding": "ENUS17"
+    },
+    {
+      "Text": "J",
+      "Encoding": "ENUS18"
+    },
+    {
+      "Text": "j",
+      "Encoding": "ENUS19"
+    },
+    {
+      "Text": "K",
+      "Encoding": "ENUS20"
+    },
+    {
+      "Text": "k",
+      "Encoding": "ENUS21"
+    },
+    {
+      "Text": "L",
+      "Encoding": "ENUS22"
+    },
+    {
+      "Text": "l",
+      "Encoding": "ENUS23"
+    },
+    {
+      "Text": "M",
+      "Encoding": "ENUS24"
+    },
+    {
+      "Text": "m",
+      "Encoding": "ENUS25"
+    },
+    {
+      "Text": "N",
+      "Encoding": "ENUS26"
+    },
+    {
+      "Text": "n",
+      "Encoding": "ENUS27"
+    },
+    {
+      "Text": "O",
+      "Encoding": "ENUS28"
+    },
+    {
+      "Text": "o",
+      "Encoding": "ENUS29"
+    },
+    {
+      "Text": "P",
+      "Encoding": "ENUS30"
+    },
+    {
+      "Text": "p",
+      "Encoding": "ENUS31"
+    },
+    {
+      "Text": "Q",
+      "Encoding": "ENUS32"
+    },
+    {
+      "Text": "q",
+      "Encoding": "ENUS33"
+    },
+    {
+      "Text": "R",
+      "Encoding": "ENUS34"
+    },
+    {
+      "Text": "r",
+      "Encoding": "ENUS35"
+    },
+    {
+      "Text": "S",
+      "Encoding": "ENUS36"
+    },
+    {
+      "Text": "s",
+      "Encoding": "ENUS37"
+    },
+    {
+      "Text": "T",
+      "Encoding": "ENUS38"
+    },
+    {
+      "Text": "t",
+      "Encoding": "ENUS39"
+    },
+    {
+      "Text": "U",
+      "Encoding": "ENUS40"
+    },
+    {
+      "Text": "u",
+      "Encoding": "ENUS41"
+    },
+    {
+      "Text": "V",
+      "Encoding": "ENUS42"
+    },
+    {
+      "Text": "v",
+      "Encoding": "ENUS43"
+    },
+    {
+      "Text": "W",
+      "Encoding": "ENUS44"
+    },
+    {
+      "Text": "w",
+      "Encoding": "ENUS45"
+    },
+    {
+      "Text": "X",
+      "Encoding": "ENUS46"
+    },
+    {
+      "Text": "x",
+      "Encoding": "ENUS47"
+    },
+    {
+      "Text": "Y",
+      "Encoding": "ENUS48"
+    },
+    {
+      "Text": "y",
+      "Encoding": "ENUS49"
+    },
+    {
+      "Text": "Z",
+      "Encoding": "ENUS50"
+    },
+    {
+      "Text": "z",
+      "Encoding": "ENUS51"
+    }
+  ],
+  "NBZM": [{
+      "Text": "0",
+      "Encoding": "NBZM00"
+    },
+    {
+      "Text": "1",
+      "Encoding": "NBZM01"
+    },
+    {
+      "Text": "2",
+      "Encoding": "NBZM02"
+    },
+    {
+      "Text": "3",
+      "Encoding": "NBZM03"
+    },
+    {
+      "Text": "4",
+      "Encoding": "NBZM04"
+    },
+    {
+      "Text": "5",
+      "Encoding": "NBZM05"
+    },
+    {
+      "Text": "6",
+      "Encoding": "NBZM06"
+    },
+    {
+      "Text": "7",
+      "Encoding": "NBZM07"
+    },
+    {
+      "Text": "8",
+      "Encoding": "NBZM08"
+    },
+    {
+      "Text": "9",
+      "Encoding": "NBZM09"
+    }
+  ],
+  "ZHCN": [{
+      "Text": "你",
+      "Encoding": "ZHCN00"
+    },
+    {
+      "Text": "我",
+      "Encoding": "ZHCN01"
+    },
+    {
+      "Text": "他",
+      "Encoding": "ZHCN02"
+    }
+  ],
+  "SYMB": [{
+      "Text": "{",
+      "Encoding": "SYMB00"
+    },
+    {
+      "Text": "}",
+      "Encoding": "SYMB01"
+    },
+    {
+      "Text": "[",
+      "Encoding": "SYMB02"
+    },
+    {
+      "Text": "]",
+      "Encoding": "SYMB03"
+    },
+    {
+      "Text": "(",
+      "Encoding": "SYMB04"
+    },
+    {
+      "Text": ")",
+      "Encoding": "SYMB05"
+    },
+    {
+      "Text": "|",
+      "Encoding": "SYMB06"
+    },
+    {
+      "Text": "&",
+      "Encoding": "SYMB07"
+    },
+    {
+      "Text": "=",
+      "Encoding": "SYMB08"
+    },
+    {
+      "Text": ".",
+      "Encoding": "SYMB09"
+    },
+    {
+      "Text": "'",
+      "Encoding": "SYMB10"
+    },
+    {
+      "Text": ";",
+      "Encoding": "SYMB11"
+    },
+    {
+      "Text": "_",
+      "Encoding": "SYMB12"
+    },
+    {
+      "Text": '"',
+      "Encoding": "SYMB13"
+    },
+    {
+      "Text": ",",
+      "Encoding": "SYMB14"
+    },
+    {
+      "Text": "<",
+      "Encoding": "SYMB15"
+    },
+    {
+      "Text": ">",
+      "Encoding": "SYMB16"
+    },
+    {
+      "Text": "+",
+      "Encoding": "SYMB17"
+    },
+    {
+      "Text": "-",
+      "Encoding": "SYMB18"
+    },
+    {
+      "Text": "*",
+      "Encoding": "SYMB19"
+    },
+    {
+      "Text": "/",
+      "Encoding": "SYMB20"
+    },
+    {
+      "Text": ":",
+      "Encoding": "SYMB21"
+    }
+  ]
+});
+
+//解码函数
+function Decode_lZiMUl(Str) {
+  let Res = new Array();
+  let Search_Value;
+  let Search_Values;
+  let StrRes = "";
+
+  function Encoding_Library(Str, Type) {
+    let Return_Value;
+
+    for (let Values = 0; Values < Encoding_lZiMUl_List.length; Values++)
+      for (let Value = 0; Value < eval("Encoding_lZiMUl_List[Values]." + Type + ".length"); Value++)
+        if (Str == eval("Encoding_lZiMUl_List[Values]." + Type + "[Value].Encoding")) {
+
+          Search_Values = Values;
+          Search_Value = Value;
+          Return_Value = true;
+          break;
+        };
+    return Return_Value;
+  };
+
+  let Values = 1;
+  for (let Value = 0; Value < Str.length; Value++) {
+    Res[Value] = Str.substring(Values, Values + 6);
+    Values += 9;
+  };
+  for (let Value = 0; Value < Res.length; Value++)
+    if (Res[Value] == "" || typeof(Res[Value]) == "undefined") {
+      Res.splice(Value, 1);
+      Value--;
+    };
+
+  for (let Value = 0; Value < Res.length; Value++)
+    if (Res[Value].substring(0, 4) == "ENUS") {
+      if (Encoding_Library(Res[Value], Res[Value].substring(0, 4)) == true) {
+        StrRes += Encoding_lZiMUl_List[Search_Values].ENUS[Search_Value].Text;
+      };
+    } else if (Res[Value].substring(0, 4) == "NBZM") {
+    if (Encoding_Library(Res[Value], Res[Value].substring(0, 4)) == true) {
+      StrRes += Encoding_lZiMUl_List[Search_Values].NBZM[Search_Value].Text;
+    };
+  } else if (Res[Value].substring(0, 4) == "ZHCN") {
+    if (Encoding_Library(Res[Value], Res[Value].substring(0, 4)) == true) {
+      StrRes += Encoding_lZiMUl_List[Search_Values].ZHCN[Search_Value].Text;
+    };
+  } else if (Res[Value].substring(0, 4) == "SYMB") {
+    if (Encoding_Library(Res[Value], Res[Value].substring(0, 4)) == true) {
+      StrRes += Encoding_lZiMUl_List[Search_Values].SYMB[Search_Value].Text;
+    };
+  } else if (Res[Value].substring(0, 8) == "??????") {
+    StrRes += "######";
+  } else if (Res[Value].substring(0, 8) == "SPACES") {
+    StrRes += " ";
+  };
+
+
+  return StrRes;
+};
+
+Main();
